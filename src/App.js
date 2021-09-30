3e3c799337f52611d8b3d7c4301d9d2de1e2d0a9
Xavi <xavi.acu.13@gmail.com>
2021-09-30 11:23:49 -0400

    add formulario component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
-  <Header 
-    title='Weanther React App'
-  />
+    <>
+      <Header 
+        title='Weanther React App'
+      />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario />
+            </div>
+            <div className="col m6 s12">
+              2
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
  );
 }
 
---
+++ src/components/Formulario.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Formulario = () => {
+  return ( 
+    <h1>This is Formulario</h1>
+   );
+}
+ 
+export default Formulario;
